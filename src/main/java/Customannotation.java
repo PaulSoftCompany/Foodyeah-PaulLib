6aa96b4d668d45ed04aaaf40cfd7b5a9a5d8d62d
Akiraki <akiraki.awa@gmail.com>
2020-06-30 14:22:51 -0500

    Se la estructura de la libreria gradle

---
+++ src/main/java/Customannotation.java
@@ -0,0 +1,15 @@
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Inherited
+@Documented
+@Target(ElementType.TYPE)
+public @interface Customannotation{
+
+}
